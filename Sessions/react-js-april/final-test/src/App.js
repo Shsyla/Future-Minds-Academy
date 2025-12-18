81605c24a638ce827ee9b69c1a5ecf17a81fcb37
Shsyla <shkurtesyla94@gmail.com>
2025-12-18 19:50:32 +0100

    Restore local changes after reconnecting to remote

---
+++ Sessions/react-js-april/final-test/src/App.js
@@ -0,0 +1,11 @@
+import logo from './logo.svg';
+import './App.css';
+import UserManagement from './UserManagement';
+
+function App() {
+  return (
+    <UserManagement />
+  );
+}
+
+export default App;
